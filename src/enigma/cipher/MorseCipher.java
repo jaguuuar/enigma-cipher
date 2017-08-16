3f16f96a7954668c9ccc81825834e76709074e82
PawelSyktus <pawel.syktus@gmail.com>
2017-08-17 01:43:39 +0200

    Change character for whitespace coding

--- src/enigma/cipher/MorseCipher.java
+++ src/enigma/cipher/MorseCipher.java
@@ -18,18 +18,34 @@ public class MorseCipher {
         for (int i = 0; i < text.length(); i ++){
           Character letter = text.charAt(i);
 
-          if (!letter.toString().equals(" ")):
-            encipheredText += morseCode.get(letter.toString()) + "~";
+          if (!letter.toString().equals("~")):
+            encipheredText += morseCode.get(letter.toString()) + "#";
         }
 
         return "######## ciphered: " + encipheredText + " #########";
 	}
 
     public String decipher(String text){
+
+        String deciphredText = "";
+        HashMap<String, String> morseCode = DataManager.loadFile("numberscipher.txt");
+
+        for (int i = 0; i < text.length(); i = i + 2){
+          Character number1 = text.charAt(i);
+          Character number2 = text.charAt(i+1);
+          String hiddenLetter = number1.toString() + number2.toString();
+
+          for (String key: keysCipher.keySet()){
+            if (keysCipher.get(key).equals(hiddenLetter)){
+              deciphredText += key;
+            }
+          }
+        }
+    	return "deciphered: "+deciphredText;
 	}
 
     public String getName(){
-		return "FakeEnigma";
+		return "MorseCipher";
 	}
 
     public boolean isKeyRequired(){
