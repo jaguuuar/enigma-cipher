ab27209a371760614b1d0274da6a41b497fd4591
Michal Janiczko <michaljaniczko@gmail.com>
2017-08-16 10:58:49 +0200

    Add decipher method

--- src/enigma/cipher/MirrorCipher.java
+++ src/enigma/cipher/MirrorCipher.java
@@ -1,48 +1,11 @@
-// package enimga;
+public class MirrorCipher{
 
-import java.util.Scanner;
-import java.lang.StringBuffer;
-// import services.EnigmaService;
+		public String encipher(String text){
+		  String reverse = new StringBuffer(text).reverse().toString();
+		  return "###### ciphered: "+reverse+" #####";
+		}
 
-
-public class MirrorCipher{		// implements EnigmaService {
-
-	public static final boolean KEY_REQUIRED = false;
-
-
-	public static void main(String[] args){
-
-		MirrorCipher mirror = new MirrorCipher();
-		Scanner input = new Scanner(System.in);
-		String text = input.nextLine();
-		String enciperText = mirror.encipher(text);
-		System.out.println(enciperText);
-
-	}
-
-	// public MirrorCipher(){
-	// }
-
-	public String encipher(String text){
-	  String reverse = new StringBuffer(text).reverse().toString();
-	  return "###### ciphered: "+reverse+" #####";
-	}
-
-	public String decipher(String text){
-	  String reverse = new StringBuffer(text).reverse().toString();
-	  return "###### deciphered: "+reverse+" #####";
-	}
-
-	public String getName(){
-	  return "MirrorCipher";
-	}
-
-	public boolean isKeyRequired(){
-	  return KEY_REQUIRED;
-	}
-
-	public void setKey(String key) {
-
-	}
-
-}
+		public String decipher(String text){
+		  String reverse = new StringBuffer(text).reverse().toString();
+		  return "###### deciphered: "+reverse+" #####";
+		}
