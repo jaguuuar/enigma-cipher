e8edb0375fc690e32ca6d936cd796e3d1f4cf0e4
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-16 14:32:53 +0200

    Make class Terminal Translator

--- src/enigma/TerminalTranslator.java
+++ src/enigma/TerminalTranslator.java
@@ -1,5 +1,11 @@
 package enigma;
 
+import app.Module;
+import services.EnigmaService;
+import registry.ServiceProvider;
+import java.util.Scanner;
+
 public class TerminalTranslator {
-    
-}
+
+	private ServiceProvider provider;
+    private String mode, cipherName, cipherKey;
