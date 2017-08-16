2a3880bf7700ea3e226139ee5cba1bd85f7cd407
PawelSyktus <pawel.syktus@gmail.com>
2017-08-16 13:14:54 +0200

    Add constructor

--- src/enigma/ServiceRepository.java
+++ src/enigma/ServiceRepository.java
@@ -1,5 +1,17 @@
 package enigma;
 
-public class ServiceRepository {
-    
+import registry.ServiceRegistry;
+import registry.ServiceProvider;
+import services.EnigmaService;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ServiceRepository implements ServiceRepository, ServiceRegistry {
+
+    LinkedList<EnigmaService> services;
+
+    public ServiceRepository(){
+        this.services = new LinkedList<EnigmaService>();
+    }
 }
