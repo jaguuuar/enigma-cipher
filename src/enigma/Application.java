925f77dcf9277070e0d01063a7b5b80c33c31707
jaguuuar <sirkamson@gmail.com>
2017-08-16 15:19:37 +0200

    track files

--- src/enigma/Application.java
+++ src/enigma/Application.java
@@ -33,7 +33,7 @@ public class Application {
 
 
     public static void showMainMenu(){
-
+        
     }
 
 }
---
+++ src/enigma/cipher/DataManager.java
@@ -0,0 +1,29 @@
+import java.util.File;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.FileNotFoundException;
+
+public class DataManager() {
+
+    public static HashMap<String, String> loadFile(String fileName){
+
+        HashMap<String, String> codeMap = new HashMap<String, String>();
+        File file = new File(fileName);
+
+        try {
+
+            Scanner read = new Scanner(file);
+
+            while (read.hasNextLine()) {
+                String line = read.nexLine();
+                String[] splittedCode = line.split("\\s\\|\\s");
+                codeMap.put(splittedCode[0], splittedCode[1]);
+            }
+            read.close();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    return codeMap;
+    }
+}
