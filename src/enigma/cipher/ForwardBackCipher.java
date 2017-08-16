c4f1782f0bf68042ec646f6dd9aaff1a8bd7c009
jaguuuar <sirkamson@gmail.com>
2017-08-16 12:35:08 +0200

    add encipher and decipher methods

--- src/enigma/cipher/ForwardBackCipher.java
+++ src/enigma/cipher/ForwardBackCipher.java
@@ -2,11 +2,11 @@ public class ForwardBackCipher
 {
     public static void main(String[] args)
     {
+        String ciphered = encipherForwardBack("DUPA");
+        System.out.println(ciphered);
 
-        char[] alphabet = makeAlphabetTable();
-        System.out.println(alphabet);
-        // encipherForwardBack("DUPA");
-
+        String ciphered2 = decipherForwardBack("ecvtqobz");
+        System.out.println(ciphered2);
     }
 
     public static char[] makeAlphabetTable()
@@ -21,31 +21,48 @@ public class ForwardBackCipher
         return alpha;
     }
 
-    // public static String encipherForwardBack(String text)
-    // {
-    //     char[] alphabet = makeAlphabetTable();
-    //     String encipherText = "";
-    //
-    //     for (int i = 0; i < text.length(); i++)
-    //     {
-    //         System.out.println(Character.toLowerCase(text.charAt(i)));
-    //         for (int j = 1; j < 27; j++)
-    //         {
-    //             char textLetter = Character.toLowerCase(text.charAt(i));
-    //             char alphabetLetter = alphabet[j];
-    //             if(textLetter == alphabetLetter )
-    //             {
-    //                 encipherText = encipherText + alphabet[i + 1];
-    //                 encipherText = encipherText + alphabet[i - 1];
-    //             }
-    //         }
-    //
-    //     }
-    //
-    //     return encipherText;
-    //
-    //
-    //
-    // }
+    public static String encipherForwardBack(String text)
+    {
+        char[] alphabet = makeAlphabetTable();
+        String encipherText = "";
+
+        for (int i = 0; i < text.length(); i++)
+        {
+            for (int j = 1; j < 27; j++)
+            {
+                char textLetter = Character.toLowerCase(text.charAt(i));
+                char alphabetLetter = alphabet[j];
+                if(textLetter == alphabetLetter )
+                {
+                    encipherText = encipherText + alphabet[j + 1];
+                    encipherText = encipherText + alphabet[j - 1];
+                }
+            }
+
+        }
+        return encipherText;
+    }
+
+
+    public static String decipherForwardBack(String text)
+    {
+        char[] alphabet = makeAlphabetTable();
+        String decipherText = "";
+
+        for (int i = 0; i < text.length(); i = i + 2)
+        {
+            for (int j = 1; j < 27; j++)
+            {
+                char textLetter = Character.toLowerCase(text.charAt(i));
+                char alphabetLetter = alphabet[j];
+                if(textLetter == alphabetLetter )
+                {
+                    decipherText = decipherText + alphabet[j - 1];
+                }
+            }
+
+        }
+        return decipherText;
+    }
 
 }
