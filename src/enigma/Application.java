c63f72bd32dd7dd4ec1bdcf8dcedc5ec77a130aa
jaguuuar <sirkamson@gmail.com>
2017-08-16 15:10:58 +0200

    implement main function

--- src/enigma/Application.java
+++ src/enigma/Application.java
@@ -8,6 +8,25 @@ public class Application {
 	public static void main(String[] args) {
         String mode = null, cipherName = null, cipherKey = null;
 
+        try {
+            mode = args[0];
+            cipherName = args[1];
+            if (args.size() == 3)
+                cipherKey = args[2];
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            showMainMenu()
+        }
+
+        ServiceRepository repo = new ServiceRepository();
+        repo.register(new cipherName());
+
+        Module module = new cipherName();
+        module.initialize(repo);
+        module.start();
+    }
+
+
 
 
 
