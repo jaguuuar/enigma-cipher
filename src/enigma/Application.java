bde7b2e921582804b7bb22d9a2561b84e4f34241
jaguuuar <sirkamson@gmail.com>
2017-08-16 15:09:32 +0200

    make sceleton of Application

---
+++ src/enigma/Application.java
@@ -0,0 +1,17 @@
+package enigma;
+
+import services.EnigmaService;
+import app.Module;
+
+public class Application {
+
+	public static void main(String[] args) {
+        String mode = null, cipherName = null, cipherKey = null;
+
+
+
+
+
+
+
+}
