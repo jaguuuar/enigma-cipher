b4d82c338f89c75d9616e2735574e27ae26d89ce
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-17 14:32:03 +0200

    Add constructor class Rot13

---
+++ src/enigma/cipher/Rot13.java
@@ -0,0 +1,11 @@
+package enigma.cipher;
+import java.util.Scanner;
+import services.EnigmaService;
+
+
+public class Rot13 implements EnigmaService{
+
+	public static final boolean KEY_REQUIRED = false;
+
+	public Rot13(){}
+}
