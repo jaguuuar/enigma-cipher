b09125fe294858eadf902afcc9eb5f35151f05a5
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-17 14:34:15 +0200

    Add method encipher()

--- src/enigma/cipher/Rot13.java
+++ src/enigma/cipher/Rot13.java
@@ -8,4 +8,14 @@ public class Rot13 implements EnigmaService{
 	public static final boolean KEY_REQUIRED = false;
 
 	public Rot13(){}
+    public String encipher(String text){
+        String rot13 = "";
+        int len = text.length();
+        for (int i = 0; i < len; i++) {
+            char c = (text.charAt(i));
+
+
+
+		}
+        return "cipher: " + rot13;
 }
