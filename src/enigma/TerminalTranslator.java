152416254d8dddbe89b16c16a46175d0965b82ab
Przemo8922 <przemyslawsalak@gmail.com>
2017-08-18 02:20:00 +0200

    Add docstring to TerminalTranslator

--- src/enigma/TerminalTranslator.java
+++ src/enigma/TerminalTranslator.java
@@ -24,6 +24,9 @@ public class TerminalTranslator {
 		return "TerminalTranslator";
 	}
 
+	/**
+	* Method to translate user input in console to use program.
+	*/
 	public void start(){
 		EnigmaService enigma = provider.getByName(cipherName);
 		Scanner scan = new Scanner(System.in);
@@ -35,16 +38,19 @@ public class TerminalTranslator {
 					System.out.println(enigma.encipher(scan.nextLine()));
 				}
 			}
+
 			else if (mode.equalsIgnoreCase("-d")){
 				while(scan.hasNextLine()){
 					System.out.println(enigma.decipher(scan.nextLine()));
 				}
 			}
+
 			else if (mode.equalsIgnoreCase("-l")){
 				for (String cipherName : provider.listAll()){
 					System.out.println(cipherName);
 				}
 			}
+
 			else {
 				System.out.println("Wrong option name!");
 				Application.showMainMenu();
